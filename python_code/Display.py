7e2290a4dd0fd42cc8567ec5b90e347d0ed68553
tyst3273 <tyst3273@colorado.edu>
2024-03-25 10:31:50 -0600

    adsdf

--- python_code/Display.py
+++ python_code/Display.py
@@ -9,22 +9,27 @@
 
 
 import os
-import PyPDF2 
-from PyPDF2 import PdfFileReader, PdfFileMerger
+from PyPDF2 import PdfReader, PdfMerger  
 from RSE_Constants import *
 
-
 class Display:
+
     def MakePlotSummary(self,folder,subdir):
+
         files_dir = folder
         name=RSE_Constants.NOT_STARTS_WITH+subdir+RSE_Constants.ALL_PLOTS
-#        if(1==1):
+
         try: 
-            pdf_files = [f for f in sorted(os.listdir(files_dir)) if f.startswith(RSE_Constants.STARTS_WITH) and f.endswith(RSE_Constants.ENDS_WITH) and not f.startswith(RSE_Constants.NOT_STARTS_WITH)]
-            merger = PdfFileMerger()
+
+            pdf_files = [f for f in sorted(os.listdir(files_dir)) if \
+                         f.startswith(RSE_Constants.STARTS_WITH) and \
+                         f.endswith(RSE_Constants.ENDS_WITH) and not \
+                         f.startswith(RSE_Constants.NOT_STARTS_WITH)]
+
+            merger = PdfMerger()
 
             for filename in pdf_files:
-                merger.append(PdfFileReader(os.path.join(files_dir, filename), "rb"))
+                merger.append(PdfReader(os.path.join(files_dir, filename), "rb"))
             
             merger.write(os.path.join(files_dir, name))
 
@@ -32,5 +37,6 @@ class Display:
 #                os.remove(os.path.join(files_dir, filename))
             
         except Exception as e:
+
             print ("MakePlotsSumary failed in "+folder)
             print(e)
